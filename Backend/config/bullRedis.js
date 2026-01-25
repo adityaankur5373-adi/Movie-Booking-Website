5dadc2aa579c173f7764566b72b94354a8de32ab
adityaankur5373-adi <adityaankur5373@gmail.com>
2026-01-26 00:36:24 +0530

    upstash

--- Backend/config/bullRedis.js
+++ Backend/config/bullRedis.js
@@ -1,9 +1,13 @@
 import IORedis from "ioredis";
 
-const bullRedis = new IORedis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6380,
-  maxRetriesPerRequest: null,
-});
+const bullRedis = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new IORedis({
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: Number(process.env.REDIS_PORT) || 6380,
+      maxRetriesPerRequest: null,
+    });
 
 export default bullRedis;
--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,9 +1,11 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6379,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL) // Upstash (prod)
+  : new Redis({
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: Number(process.env.REDIS_PORT) || 6379,
+    });
 
 redis.on("connect", () => console.log("✅ Redis connected"));
 redis.on("error", (err) => console.log("❌ Redis error:", err));
