6de44582af28b5dea8fdf8338309245f2d07d532
adityaankur5373-adi <adityaankur5373@gmail.com>
2026-01-31 23:37:13 +0530

    fixes

--- view/src/components/TrailerSection.jsx
+++ view/src/components/TrailerSection.jsx
@@ -88,7 +88,7 @@ const TrailerSection = () => {
           <>
             {embedUrl ? (
               <ReactPlayer
-                url={embedUrl}   {/* ✅ FIXED */}
+                url={embedUrl}   
                 controls
                 className="mx-auto max-w-full"
                 width="960px"
